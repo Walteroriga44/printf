ba82b5d1bc8f93b2f0fe91d456f2fbad6e454ab3
Walteroriga44 <133379417+Walteroriga44@users.noreply.github.com>
2023-07-25 09:53:01 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
 
 void print_buffer(char buffer[], int *buff_ind);
@@ -67,4 +66,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
